3e30e3c8aa447152762d9db083181c66f57f28d4
Oliver Wang Hansen <olha@nodesagency.com>
2016-11-21 17:34:19 +0000

    Deleted codebase

--- sandbox/Sources/UAPusher/Drop+UAPusher.swift
+++
@@ -1,8 +0,0 @@
-import Vapor
-
-extension Droplet {
-    public var uapusher: UAPusher? {
-        get { return storage["uapusher"] as? UAPusher }
-        set { storage["uapusher"] = newValue }
-    }
-}
