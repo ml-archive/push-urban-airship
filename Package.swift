856776d0b84574e499728c07d9c5427c2ab537b4
Oliver Wang Hansen <olha@nodesagency.com>
2016-11-21 17:37:34 +0000

    Updated Package

--- Package.swift
+++ Package.swift
@@ -1,23 +1,8 @@
 import PackageDescription
 
 let package = Package(
-    name: "sandbox",
-    targets: [
-        Target(name: "UAPusher"),
-        Target(name: "App", dependencies: [
-            .Target(name: "UAPusher")
-            ])
-    ],
+    name: "UAPusher",
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 1)
-    ],
-    exclude: [
-        "Config",
-        "Database",
-        "Localization",
-        "Public",
-        "Resources",
-        "Tests",
-    ]
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1),
+        ]
 )
-
