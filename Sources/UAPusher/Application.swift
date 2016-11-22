6193e8fe12aefa468d07d285e963a0f0220cb5c9
Oliver Wang Hansen <olha@nodesagency.com>
2016-11-22 10:44:00 +0000

    Added application

---
+++ Sources/UAPusher/Application.swift
@@ -0,0 +1,13 @@
+final class Application {
+    
+    let masterSecret: String
+    let appKey: String
+    
+    public init(masterSecret: String, appKey: String) {
+        
+        self.masterSecret = masterSecret
+        self.appKey = appKey
+        
+    }
+    
+}
