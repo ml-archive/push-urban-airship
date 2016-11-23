8ef784ac5a96a3b61da1d28b231917a2c13ec1e3
Oliver Wang Hansen <olha@nodesagency.com>
2016-11-23 17:07:41 +0000

    Added validate method and updated send

--- Sources/UAPusher/UAPusher.swift
+++ Sources/UAPusher/UAPusher.swift
@@ -23,11 +23,44 @@ public final class UAPusher {
         self.drop = drop
     }
     
+    //MARK: Send
+    
     public func send(request: UARequest) throws -> Status {
         
         //Add more defensive coding here
         
         let response = try self.connectionManager.post(slug: "/api/push", body: request.getBody())
+        return response
+        
+    }
+    
+    public func send(request: JSON) throws -> Status {
+        
+        //Add more defensive coding here
+        
+        let response = try self.connectionManager.post(slug: "/api/push", body: request)
+        return response
+        
+    }
+    
+    //MARK: Validate
+    
+    public func validateUASetup() throws -> Status {
+        
+        let body = try JSON(node: [
+            "audience": "all",
+            "device_types": [
+                "ios",
+                "android"
+            ],
+            "notification": [
+                "ios": [
+                    "alert": "VAPOR test"
+                ]
+            ]
+        ])
+        
+        let response = try self.connectionManager.post(slug: "/api/push/validate", body: body)
         return response
         
     }
