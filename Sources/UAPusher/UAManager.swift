f6831a56cb024269437fd067929d7a00115af953
Rasmus Ebbesen <re@nodes.dk>
2017-12-29 15:47:03 +0100

    removed some debugging lines

--- Sources/UAPusher/UAManager.swift
+++ Sources/UAPusher/UAManager.swift
@@ -31,11 +31,7 @@ public final class UAManager {
     }
     
     public func send(request: UARequest) throws -> UAResponse {
-        // For debugging
-        try print(request.getBody().makeResponse())
-        
         let responses = try self.connectionManager.post(slug: "/api/push", content: request.getBody())
-        
         let uaResponse = UAResponse(responses: responses)
         
         if uaResponse.status != .accepted {
